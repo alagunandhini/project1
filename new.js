13cfc1122555c377d26694fad968fc33fdcca264
alagunandhini <alagunadhini@gmail.com>
2025-01-28 23:52:21 +0530

    changed some

---
+++ new.js
@@ -0,0 +1,10 @@
+var overlay1=document.querySelector(".overlay")
+var forms=document.querySelector(".form-1")
+
+var plusbutton=document.getElementById("plus")
+plusbutton.addEventListener("click",function(event){
+overlay1.style.display="block";
+forms.style.display="block";
+
+
+})
